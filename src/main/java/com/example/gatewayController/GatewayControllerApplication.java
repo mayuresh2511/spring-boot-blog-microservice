417e8174208b45c6a1125691949a37d8dfbef493
Mayuresh Marathe <mayuresh.marathe123@gmail.com>
2024-05-19 13:54:12 +0530

    Initial Commit

---
+++ src/main/java/com/example/gatewayController/GatewayControllerApplication.java
@@ -0,0 +1,12 @@
+package com.example.gatewayController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayControllerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/gatewayController/GatewayControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
